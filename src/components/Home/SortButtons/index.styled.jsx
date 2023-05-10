5231473f3ac0bad58ef8e688648b0947b720cb77
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-11 01:50:10 +0200

    added border for mobile view

--- src/components/Home/SortButtons/index.styled.jsx
+++ src/components/Home/SortButtons/index.styled.jsx
@@ -6,11 +6,17 @@ export const SortButtonContainer = styled.div`
   gap: 10px;
   text-align: center;
   margin: 20px auto 0px;
+  border-bottom: 1px solid ${(props) => props.theme.color.borders};
+
+  @media (min-width: 1000px) {
+    border-bottom: none;
+  }
 
   button {
     width: 145px;
     font-size: 1rem;
     box-shadow: ${(props) => props.theme.boxShadow};
+    margin-bottom: 40px;
 
     &.active {
       outline: 2px solid ${(props) => props.theme.color.accentTwo};
