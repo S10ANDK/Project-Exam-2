22e944af9ceaefd699d1d14ba5d0e749007daa73
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-11 16:15:15 +0200

    Added folders and files for all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import Register from './components/Register';
+import RegisterVenueManager from './components/RegisterVenueManager';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+      </Route>
+      <Route path="/register" element={<Layout />}>
+        <Route index element={<Register />} />
+      </Route>
+      <Route path="/register-venue-manager" element={<Layout />}>
+        <Route index element={<RegisterVenueManager />} />
       </Route>
     </Routes>
   );
---
+++ src/components/RegisterVenueManager/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RegisterVenueManager() {
+  return <div></div>;
+}
+
+export default RegisterVenueManager;
