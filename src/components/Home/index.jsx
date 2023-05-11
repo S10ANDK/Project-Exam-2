4366e2dccd3f9c7d63d99a80be8f6cc679d04c75
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-11 15:53:39 +0200

    installed helmet for title and description

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,6 +7,7 @@ import Search from './Search';
 import PriceFilter from './PriceFilter';
 import MobileFilterButton from './MobileFilterButton';
 import MaxGuestsFilter from './MaxGuestsFilter';
+import { Helmet } from 'react-helmet';
 
 /*
   Renders the home page with all its components
@@ -23,52 +24,61 @@ function Home() {
   const [pageIndex, setPageIndex] = useState(0);
 
   return (
-    <S.HomeContainer>
-      <h1>discover your next adventure</h1>
-      <MobileFilterButton
-        onButtonClick={() => setFiltersVisible((prev) => !prev)}
-      />
-      <S.SearchFiltersContainer filtersVisible={filtersVisible}>
-        <div>
-          <Search onSearch={setSearchTerm} minLength={3} />
-          <FilterButtons
-            filters={filters}
-            setFilters={setFilters}
-            maxGuests={maxGuests}
-            setMaxGuests={setMaxGuests}
+    <>
+      <Helmet>
+        <title>browse | holidaze</title>
+        <meta
+          name="description"
+          content="Discover you next adventure with Holidaze, renting bnb's!"
+        />
+      </Helmet>
+      <S.HomeContainer>
+        <h1>discover your next adventure</h1>
+        <MobileFilterButton
+          onButtonClick={() => setFiltersVisible((prev) => !prev)}
+        />
+        <S.SearchFiltersContainer filtersVisible={filtersVisible}>
+          <div>
+            <Search onSearch={setSearchTerm} minLength={3} />
+            <FilterButtons
+              filters={filters}
+              setFilters={setFilters}
+              maxGuests={maxGuests}
+              setMaxGuests={setMaxGuests}
+              setPageIndex={setPageIndex}
+            />
+          </div>
+          <div>
+            <MaxGuestsFilter
+              maxGuests={maxGuests}
+              setMaxGuests={setMaxGuests}
+              setPageIndex={setPageIndex}
+            />
+            <PriceFilter
+              setMinPrice={setMinPrice}
+              setMaxPrice={setMaxPrice}
+              setPageIndex={setPageIndex}
+            />
+          </div>
+          <SortButtons
+            sortOrder={sortOrder}
+            setSortOrder={setSortOrder}
             setPageIndex={setPageIndex}
           />
-        </div>
-        <div>
-          <MaxGuestsFilter
-            maxGuests={maxGuests}
-            setMaxGuests={setMaxGuests}
-            setPageIndex={setPageIndex}
-          />
-          <PriceFilter
-            setMinPrice={setMinPrice}
-            setMaxPrice={setMaxPrice}
-            setPageIndex={setPageIndex}
-          />
-        </div>
-        <SortButtons
+        </S.SearchFiltersContainer>
+        <DisplayVenueList
+          filters={filters}
           sortOrder={sortOrder}
-          setSortOrder={setSortOrder}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          minPrice={minPrice}
+          maxPrice={maxPrice}
+          maxGuests={maxGuests}
+          pageIndex={pageIndex}
           setPageIndex={setPageIndex}
         />
-      </S.SearchFiltersContainer>
-      <DisplayVenueList
-        filters={filters}
-        sortOrder={sortOrder}
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        minPrice={minPrice}
-        maxPrice={maxPrice}
-        maxGuests={maxGuests}
-        pageIndex={pageIndex}
-        setPageIndex={setPageIndex}
-      />
-    </S.HomeContainer>
+      </S.HomeContainer>
+    </>
   );
 }
 
