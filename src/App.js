6ab93d1204f23cfb3ffd1ec94b9e19ab406a67b7
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-11 16:26:18 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import Register from './components/Register';
 import RegisterVenueManager from './components/RegisterVenueManager';
+import VenueSpecific from './components/VenueSpecific';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-      </Route>
-      <Route path="/register" element={<Layout />}>
-        <Route index element={<Register />} />
-      </Route>
-      <Route path="/register-venue-manager" element={<Layout />}>
-        <Route index element={<RegisterVenueManager />} />
+
+        <Route path="venues/:id" index element={<VenueSpecific />} />
+
+        <Route path="register" index element={<Register />} />
+
+        <Route
+          path="register-venue-manager"
+          index
+          element={<RegisterVenueManager />}
+        />
       </Route>
     </Routes>
   );
---
+++ src/components/VenueSpecific/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Container from '../styles/Container/index.styled';
+
+function VenueSpecific() {
+  return <Container></Container>;
+}
+
+export default VenueSpecific;
