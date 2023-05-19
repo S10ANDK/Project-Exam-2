e1bf6de318858841e14999b422355ee16e04c3fa
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-19 15:03:28 +0200

    commented code for use later down the line

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -9,4 +9,13 @@ function Dashboard() {
   return <Container></Container>;
 }
 
+// const user = JSON.parse(localStorage.getItem('user'));
+// const accessToken = user && user.accessToken;
+// console.log(accessToken);
+// const options = {
+//   headers: {
+//     Authorization: `Bearer ${accessToken}`,
+//   },
+// };
+
 export default Dashboard;
