c9f05c7fb007b98496d49d75d1ee8c4226ff3482
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-22 17:01:32 +0200

    added helmet

--- src/components/BookingSuccess/index.jsx
+++ src/components/BookingSuccess/index.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import Container from '../styles/Container/index.styled';
 import { useParams } from 'react-router-dom';
 import * as S from './index.styled';
-
-// const navigate = useNavigate();
-
-// const handleContinue = () => {
-//   navigate('/');
-// };
+import Helmet from 'react-helmet';
 
 function BookingSuccess() {
   const { id } = useParams();
 
   return (
-    <Container>
-      <h1>Booking Successful!</h1>
-      <S.IdContainer>
-        <p>
-          Your booking ID is: <span>{id}</span>
-        </p>
-      </S.IdContainer>
-      <S.ButtonContainer>
-        <S.ContinueButton to="/">Continue</S.ContinueButton>
-      </S.ButtonContainer>
-    </Container>
+    <>
+      <Helmet>
+        <title>booking success | holidaze</title>
+        <meta name="description" content="Booking success page" />
+      </Helmet>
+
+      <Container>
+        <h1>Booking Successful!</h1>
+        <S.IdContainer>
+          <p>
+            Your booking ID is: <span>{id}</span>
+          </p>
+        </S.IdContainer>
+        <S.ButtonContainer>
+          <S.ContinueButton to="/">Continue</S.ContinueButton>
+        </S.ButtonContainer>
+      </Container>
+    </>
   );
 }
 
