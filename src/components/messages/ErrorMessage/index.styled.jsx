533e4f40b32c99f246aeda209628969c3f967ea3
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-24 19:36:38 +0200

    styling improvement for error message

--- src/components/messages/ErrorMessage/index.styled.jsx
+++ src/components/messages/ErrorMessage/index.styled.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import Container from '../../styles/Container/index.styled';
 
-export const ErrorMessageContainer = styled.div`
+export const ErrorMessageContainer = styled(Container)`
   text-align: center;
   margin-top: 70px;
   color: ${(props) => props.theme.color.secondary};
