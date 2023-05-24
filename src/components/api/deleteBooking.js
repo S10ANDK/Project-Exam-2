bfdfc56d23f2ea75fe39896e91be609772b3c342
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-24 19:06:22 +0200

    delete booking function

---
+++ src/components/api/deleteBooking.js
@@ -0,0 +1,23 @@
+import { API_URL, API_BOOKINGS } from '../constants/urls';
+import accessToken from './localStorage/accessToken';
+
+/*
+    Function for deleting booking, DELETE request
+*/
+
+async function deleteVenue(id) {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await fetch(`${API_URL}${API_BOOKINGS}/${id}`, options);
+
+  if (!response.ok) {
+    throw new Error(`Failed to delete venue: ${response.status}`);
+  }
+}
+
+export default deleteVenue;
