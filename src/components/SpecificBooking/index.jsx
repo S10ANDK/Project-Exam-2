3ed9032d6eb30935bafe287221e7fe3b62aa0826
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-24 17:44:28 +0200

    specific booking link added

---
+++ src/components/SpecificBooking/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../styles/Container/index.styled';
+
+/*
+  Specific Booking component
+*/
+
+function SpecificBooking() {
+  return <Container></Container>;
+}
+
+export default SpecificBooking;
