86b84bf0620068b96d5aaaf16e9dc55d497d0329
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2023-05-27 19:12:10 +0200

    removed commented code

--- src/components/VenueSpecific/GetSpecificVenue/BookingCard/index.styled.jsx
+++ src/components/VenueSpecific/GetSpecificVenue/BookingCard/index.styled.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
 import { StyledButtonBlue } from '../../../styles/Button/index.styled';
 
 export const BookingCard = styled.div`
